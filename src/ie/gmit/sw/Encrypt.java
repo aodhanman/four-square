03dca83f3ccfcd751ec3e2904bea40d43f703e73
aodhanman <aodhanman@gmail.com>
2018-04-08 21:36:04 +0100

    Java project

--- src/ie/gmit/sw/Encrypt.java
+++ src/ie/gmit/sw/Encrypt.java
@@ -2,21 +2,35 @@ package ie.gmit.sw;
 
 public class Encrypt {
     
-    char[] letters = {'a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+public static char[] lettersArray = "ABCDEFGHIKLMNOPQRSTUVWXYZ".toCharArray();
 
-    char[][] square1 = new char[5][5] ;
-    
+public static char[][] polySquare = new char[5][5];
 
+public static char[][] fillchar(char[][] filled) {
+    //fills char array with letters
     
-    public  char[][] fillchar(char[][] filled) {
     int x = 0;
     for(int i = 0; i < 5; i++){
              for(int j = 0; j < 5; j++){
-                filled[i][j] = letters[x];
+                filled[i][j] = lettersArray[x];
                 x++;
                 }   
             }
-    //square4 = square1.clone();
+    // O(n^2) time nested for loops
     return filled;
+    }
+public static String[] bigram(String text) {
+    //checks that text is even length, if not adds x to end
+    if (text.length() % 2 != 0) {
+        text = text + "X";
+    }
+    String[] bigrams = new String[text.length() / 2];
+    int num = 0;
+    for (int i = 0; i < (text.length() / 2); i++) {
+        bigrams[i] = text.substring(num, num + 2);
+        num = num + 2;
+    }
+    // O(n) time
+    return bigrams;
     }
 }
