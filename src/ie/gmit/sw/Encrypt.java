9de8cdd153ea3e96eea61239416e7ea86115a32f
aodhanman <aodhanman@gmail.com>
2018-04-08 23:02:15 +0100

    Java project

--- src/ie/gmit/sw/Encrypt.java
+++ src/ie/gmit/sw/Encrypt.java
@@ -4,7 +4,8 @@ public class Encrypt {
     
 public static char[] lettersArray = "ABCDEFGHIKLMNOPQRSTUVWXYZ".toCharArray();
 
-public static char[][] polySquare = new char[5][5];
+public static char[][] polySquare1 = new char[5][5];
+public static char[][] polySquare2 = new char[5][5];
 
 public static char[][] fillchar(char[][] filled) {
     //fills char array with letters
@@ -33,4 +34,27 @@ public static String[] bigram(String text) {
     // O(n) time
     return bigrams;
     }
+
+public static char[][] convertChar(char[][] conv, String input) {
+    //finds matching chars in a char[][] which will be one of the polybius squares 
+    //and a string which will be the key converted to a char array below
+    
+    //counter
+    int z = 0;
+    //convert inputted string to char array
+    char[] transform = input.toCharArray(); 
+    if(z <2){
+    for(int i = 0; i < 5; i++){
+             for(int j = 0; j < 5; j++){
+                if(transform[z] == conv[i][j]){
+                    System.out.println("x: " + i + " y: " + j);
+                
+                z++;
+                }
+                }   
+            }
+    // O(n^3) time nested for loops and if
+    }    return conv;
+    }
+
 }
