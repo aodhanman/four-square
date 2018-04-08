d89267826746990ec970263980453b92a27ce108
aodhanman <aodhanman@gmail.com>
2018-04-08 23:54:14 +0100

    Java project ya

--- src/ie/gmit/sw/Reader.java
+++ src/ie/gmit/sw/Reader.java
@@ -1,25 +1,57 @@
 package ie.gmit.sw;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.Scanner;
 
 public class Reader {
     
-    Scanner sc = new Scanner(System.in);
     
-    String select = sc.next();
-    File file = new File("PoblachtNaHEireann.txt");
-    //File file = new File("PoblachtNaHEireann.txt");
+       static String content= "";
 
-    public Reader() {
+    public  Reader() throws IOException {
     
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            System.out.println(line);
+        System.out.println("Enter text file path including name and extension");
+        
+        Scanner sc = new Scanner(System.in);
+    
+        String select = sc.nextLine();
+        String fileName = select; 
+            StringBuilder sb = new StringBuilder();
+    
+        String line = "";
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+// C:\Users\aodhan\Documents\NetBeansProjects\four-square\src\ie\gmit\sw\pob.txt
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                System.out.println("");
+                sb.append(line);
+            }   
+                content = sb.toString();
+                
+                content = content.replaceAll("[^a-zA-Z]", "").toUpperCase();
+                
+                System.out.println(content);
+
+            bufferedReader.close();         
         }
-        sc.close();
-    
-    
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+        }
+        
     }
 }
